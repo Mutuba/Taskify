d65994cb2f05cc73e35a2269759c5ac6aa3ad808
Mutuba <danielmutubait@gmail.com>
2023-01-08 11:03:22 +0300

    add toggle method to tasks controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
-  root "tasks#index"
-  
+  root 'tasks#index'
+
   resources :tasks
-  post "tasks/:id/toggle", to: "tasks#toggle"
+  post 'tasks/:id/toggle', to: 'tasks#toggle'
 end
